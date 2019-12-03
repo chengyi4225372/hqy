192c3dc286d402e589f6ff47e3ee114a1dd65feb
zhanghan <zhanghan@qq.com>
2019-12-03 16:35:43 +0800

    '样式'

--- public/static/home/js/index.js
+++ public/static/home/js/index.js
@@ -79,34 +79,33 @@ window.onload = function () {
 
   // 导航栏样式切换
 
-  // let menuList = document.querySelectorAll('#headerContent ul li')
-  // menuList.forEach((item, index) => {
-  //   item.onmouseover = function (event) {
-  //     let li = document.getElementsByClassName('nav-active')[0]
-  //     li.classList.remove('nav-active')
-  //     item.classList.add('nav-active')
-  //   }
-  //   item.onmouseout = function (event) {
-  //     item.classList.remove('nav-active')
-  //     menuList[0].classList.add('nav-active')
-  //   }
-  // })
-
   let menuList = document.querySelectorAll('#headerContent ul li')
-  let menuUl = document.querySelector('#headerContent ul')
   menuList.forEach((item, index) => {
-    item.onmouseenter = function (event) {
+    item.onmouseover = function (event) {
       let li = document.getElementsByClassName('nav-active')[0]
       li.classList.remove('nav-active')
       item.classList.add('nav-active')
     }
+    item.onmouseout = function (event) {
+      item.classList.remove('nav-active')
+      menuList[0].classList.add('nav-active')
+    }
   })
-  menuUl.onmouseleave = function () {
-    console.log(111);
-    let li = document.getElementsByClassName('nav-active')[0]
-    li.classList.remove('nav-active')
-    menuList[0].classList.add('nav-active')
-  }
+
+  // let menuList = document.querySelectorAll('#headerContent ul li')
+  // let menuUl = document.querySelector('#headerContent ul')
+  // menuList.forEach((item, index) => {
+  //   item.onmouseenter = function (event) {
+  //     let li = document.getElementsByClassName('nav-active')[0]
+  //     li.classList.remove('nav-active')
+  //     item.classList.add('nav-active')
+  //   }
+  // })
+  // menuUl.onmouseleave = function () {
+  //   let li = document.getElementsByClassName('nav-active')[0]
+  //   li.classList.remove('nav-active')
+  //   menuList[0].classList.add('nav-active')
+  // }
 
 };
 
