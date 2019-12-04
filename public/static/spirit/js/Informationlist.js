9b2139fa7a10accb02e6c59450bc028d93c14863
zhanghan <zhanghan@qq.com>
2019-12-04 14:23:46 +0800

    'yangshi'

--- public/static/spirit/js/Informationlist.js
+++ public/static/spirit/js/Informationlist.js
@@ -15,6 +15,8 @@ window.onload = function () {
         }
     }
 
+    
+
     // 返回顶部
     window.onscroll = function () {
         var top = document.body.scrollTop || document.documentElement.scrollTop;
---
+++ runtime/temp/932148a8b9897ed390ec96a321d22633.php
@@ -0,0 +1,13 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:69:"C:\phpEnv\www\hqy_\public/../application/home\view\optimal\index.html";i:1575369601;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>惠优税</title>
+</head>
+<body>
+    <div style="width:100%;text-align:center;">
+        <img src="/static/error_pic.png" style="width:1280px;height:800px;">
+    </div>
+</body>
+</html>
