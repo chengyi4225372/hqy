97bdb7f62b53f30954b3adba59bf443ab2c9e39c
‘jason <syl_880921@163.com>
2019-12-09 10:52:48 +0800

    提交

--- application/v1/controller/systematic/System.php
+++ application/v1/controller/systematic/System.php
@@ -12,56 +12,6 @@ use think\Config;
 class System extends AuthController
 {
     /**
-     * @DESC：菜单管理列表
-     * @author: jason
-     * @date: 2019-07-25 02:07:22
-     */
-    public function menu()
-    {
-        //配置
-        $status = Config::get('layout.status');
-        $type = Config::get('layout.type');
-        $params = request()->param();
-        //菜单列表
-        $list = Systems::instance()->menu($params);
-        $menuListInfo = Systems::instance()->getMenuTree();
-        $menuList = array_column($menuListInfo,'title','id');
-        $this->assign('menuList',$menuList);
-        $this->assign('status',$status);
-        $this->assign('type',$type);
-        $this->assign('data',$list['list']['data']);
-        $this->assign('page',$list['page']);
-        return $this->fetch();
-    }
-
-    /**
-     * @DESC：添加菜单页面
-     * @return mixed
-     * @author: jason
-     * @date: 2019-08-28 02:48:15
-     */
-    public function addmenu()
-    {
-        return $this->fetch();
-    }
-
-    /**
-     * @DESC：添加或更新菜单
-     * @author: jason
-     * @date: 2019-08-28 02:48:38
-     */
-    public function savemenu()
-    {
-        if($this->request->isAjax() && $this->request->isPost()){
-            $params = $_POST['data'];
-            parse_str($params,$post);
-            $return_data = Systems::instance()->savemenu($post);
-            return $return_data;
-        }
-    }
-
-
-    /**
      * @DESC：系统设置
      * @author: jason
      * @date: 2019-10-16 04:35:35
